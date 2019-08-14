838b13643e871cd98fecc780308cc9f524bc4b5c
Joscelyn <JoscelynRose@gmail.com>
2019-08-14 17:02:52 -0400

    WIP: setting up store

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import AdditionalFeature from './AdditionalFeature';
+import React from "react";
+import AdditionalFeature from "./AdditionalFeature";
 
 const AdditionalFeatures = props => {
   return (
     <div className="content">
       <h4>Additional Features</h4>
-      {props.store.length ? (
+      {/*props.store.length ? (
         <ol type="1">
           {props.store.map(item => (
             <AdditionalFeature key={item.id} feature={item} />
@@ -13,7 +13,7 @@ const AdditionalFeatures = props => {
         </ol>
       ) : (
         <p>Nice looking car!</p>
-      )}
+      )*/}
     </div>
   );
 };
--- src/components/Total.js
+++ src/components/Total.js
@@ -1,11 +1,24 @@
-import React from 'react';
-
+import React from "react";
+import { connect } from "react-redux";
 const Total = props => {
   return (
     <div className="content">
-      <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
+      <h4>Total Amount: ${/*props.car.price + props.additionalPrice*/}</h4>
     </div>
   );
 };
 
 export default Total;
+
+const mapStateToProps = state => {
+  console.log("state", state);
+  return {
+    titleOnProps: state.title,
+    editing: state.editing
+  };
+};
+
+// export default connect(
+//   mapStateToProps,
+//   { toggleTitleEditor, updateTitle }
+// )(Title);
